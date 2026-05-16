c08989f6ad91159c0b89c0f75ebeded8ae6d161f
gabrielvilarinho10 <gabriel.vilarinho@estudante.ifto.edu.br>
2026-05-16 11:42:48 -0300

    Pronto

---
+++ src/main/java/exercicios/CidadeRecord.java
@@ -0,0 +1,8 @@
+package exercicios;
+
+/**
+ * Record imutável que representa uma Cidade.
+ * Criado para garantir a imutabilidade solicitada nas especificações.
+ */
+public record CidadeRecord(String nome) {
+}
